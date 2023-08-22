c0aceebb9a590c40e6ca7d528d6ff411c0ab0801
bilalm354 <bilalm354@gmail.com>
2023-08-22 22:11:25 +0100

    renders list of shortcuts

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getWebviewContent } from './functions/getWebviewContent';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "keyboard-shortcut-helper" is now active!');
+	const provider = new KeyboardShortcutViewProvider(context.extensionUri);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('keyboard-shortcut-helper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Keyboard Shortcut Helper hehe!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(KeyboardShortcutViewProvider.viewType, provider));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class KeyboardShortcutViewProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = 'keyboardShortcutsView';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		_context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+
+		webviewView.webview.html = getWebviewContent();
+
+		webviewView.webview.onDidReceiveMessage(data => {
+			console.log('onDidReceiveMessage', data);
+		});
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
